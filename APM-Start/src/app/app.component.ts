9f30907ffa3391775ca7d06aa29973dd6c4286d7
Christon-Khong <christon.khong@gmail.com>
2020-01-13 10:01:43 -0700

    10-11

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,33 +1,79 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { AuthService } from './user/auth.service';
-import { Router } from '@angular/router';
+import { AuthService } from "./user/auth.service";
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from "@angular/router";
+import { slideInAnimation } from "./app.animation";
+import { MessageService } from "./messages/message.service";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "pm-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
-  pageTitle = 'Acme Product Management';
+  pageTitle = "Acme Product Management";
+  loading = true;
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
+  }
+
+  get isMessageDisplayed(): boolean {
+    return this.messageService.isDisplayed;
   }
 
   get userName(): string {
     if (this.authService.currentUser) {
       return this.authService.currentUser.userName;
     }
-    return '';
+    return "";
   }
 
-  constructor(private authService: AuthService,
-              private router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private messageService: MessageService
+  ) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (
+      routerEvent instanceof NavigationEnd ||
+      routerEvent instanceof NavigationCancel ||
+      routerEvent instanceof NavigationError
+    ) {
+      this.loading = false;
+    }
+  }
 
   logOut(): void {
     this.authService.logout();
-    console.log('Log out');
-    this.router.navigateByUrl('/welcome');
+    console.log("Log out");
+    this.router.navigateByUrl("/welcome");
+  }
+
+  displayMessages(): void {
+    this.router.navigate([{ outlets: { popup: ["messages"] } }]);
+    this.messageService.isDisplayed = true;
+  }
+
+  hideMessages(): void {
+    this.router.navigate([{ outlets: { popup: null } }]);
+    this.messageService.isDisplayed = false;
   }
 }
